154d40f5cdbd5201980e2a1468e37082efe9f317
andersonsouzamoreira <147444027+andersonsouzamoreira@users.noreply.github.com>
2025-06-03 14:47:13 -0300

    03/06

--- main.js
+++ main.js
@@ -15,7 +15,21 @@ for (let j=0; j<botoes.length; j++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempo0bjetivo1 = new Date("2025-12-19T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2025-12-19T00:00:00");
 
-contadores[0].textContent = tempo0bjetivo1-tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo1) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo-tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return(dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundo");
+}
